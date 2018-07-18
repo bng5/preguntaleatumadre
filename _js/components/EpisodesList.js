abc376f671d6b32de71ce7cd5c295f774a909579
Pablo Bangueses <pablo@bng5.net>
2018-07-18 16:40:32 -0300

    Cargando desde RSS. [WIP]

--- _js/components/EpisodesList.js
+++ _js/components/EpisodesList.js
@@ -10,8 +10,46 @@ class EpisodesList extends React.Component {
       playerState: 0,
       playing: null,
       progress: 0,
+      loadMore: true//props.loadMore,
     };
     this.playing = null;
+    this.load = this.load.bind(this);
+  }
+
+  load() {
+    fetch('/podcast.xml')
+    .then(response => response.text())
+    .then(text => (new window.DOMParser()).parseFromString(text, "text/xml"))
+    .then(data => {
+      window.rdata = data;
+      let episodes = this.state.episodes;
+      const items = data.getElementsByTagName('item');
+      for (let i = 0; i < items.length; i++) {
+        let rssItem = items.item(i);
+        let enclosure = rssItem.getElementsByTagName('enclosure')[0];
+        window.idata = rssItem;
+        episodes.push({
+          title: rssItem.getElementsByTagName('title')[0].firstChild.nodeValue,
+          date: "2018-01-01 00:00:00 -0300",
+          duration: rssItem.getElementsByTagNameNS('http://www.itunes.com/dtds/podcast-1.0.dtd', 'duration'),
+          episode: rssItem.getElementsByTagNameNS('http://www.itunes.com/dtds/podcast-1.0.dtd', 'episode'),
+          fecha: "0 de julio, 2018",
+          file: "file",
+          length: enclosure.getAttribute('length'),
+          url: enclosure.getAttribute('url'),
+        });
+  // <item>
+  //  <pubDate>Wed, 18 Jul 2018 10:40:00 -0300</pubDate>
+  //  <category>Podcast</category>
+  //  <guid isPermaLink="true">http://0.0.0.0:4000/episodios/patum-2018-07-11.mp3</guid>
+  //  <enclosure url="http://0.0.0.0:4000/episodios/patum-2018-07-11.mp3" length="51649877" type="audio/mpeg" />
+  //  <itunes:duration>53:48</itunes:duration>
+  //  <itunes:explicit>clean</itunes:explicit>
+  // </item>
+      }
+      this.setState({ episodes });
+    })
+    .catch(err => console.error(err));
   }
 
   togglePlay(key) {
@@ -51,22 +89,30 @@ class EpisodesList extends React.Component {
   }
 
   render() {
-    return this.state.episodes.map((episode, i) => (
-      <Episode
-        key={ i }
-        playerState={ this.state.playing === i ? this.state.playerState : 0 }
-        progress={ this.state.playing === i ? this.state.progress : 0 }
-        title={ episode.title }
-        file={ episode.file }
-        date={ episode.date }
-        duration={ episode.duration }
-        episode={ episode.episode }
-        fecha={ episode.fecha }
-        length={ episode.length }
-        playHandler={this.togglePlay.bind(this, i)}
-        sharer={this.share.bind(this, episode.url, episode.title)}
-      ></Episode>
-    ));
+    return (
+      <div>
+        {this.state.episodes.map((episode, i) => (
+          <Episode
+            key={ i }
+            playerState={ this.state.playing === i ? this.state.playerState : 0 }
+            progress={ this.state.playing === i ? this.state.progress : 0 }
+            title={ episode.title }
+            file={ episode.file }
+            date={ episode.date }
+            duration={ episode.duration }
+            episode={ episode.episode }
+            fecha={ episode.fecha }
+            length={ episode.length }
+            playHandler={this.togglePlay.bind(this, i)}
+            sharer={this.share.bind(this, episode.url, episode.title)}
+          ></Episode>
+        ))}
+        {
+          this.state.loadMore
+            ? <div className="pagination" onClick={ this.load }><button>pagination</button></div>
+            : null
+        }
+      </div>);
   }
 }
 
--- _js/entry.js
+++ _js/entry.js
@@ -6,7 +6,7 @@ import EpisodesList from './components/EpisodesList';
 var playerInstance = ReactDOM.render(<Player />, document.getElementById('player-container'));
 
 var episodes = JSON.parse(document.getElementById('data').firstChild.textContent);
-ReactDOM.render(<EpisodesList player={playerInstance} episodes={episodes} />, document.getElementById('programas'));
+ReactDOM.render(<EpisodesList player={playerInstance} episodes={episodes.programas} loadMore={episodes.load_more} />, document.getElementById('programas'));
 
 // document.getElementById('radio-playback').addEventListener('click', function (ev) {
 //   ev.preventDefault();
